7b512a2b2d3edaebe246fc775bf1a24ea0b8e8c3
Joel Lord <joelphy@gmail.com>
2019-01-15 09:06:22 -0500

    Added blank config.js files

---
+++ config.js
@@ -0,0 +1,8 @@
+// Needs to be added to the repo
+
+module.exports = {
+  VOICEIT_API_KEY: "key_XXXXXXXX",
+  VOICEIT_API_TOKEN: "tok_XXXXXXXX",
+  AUTH0_DOMAIN: "https://TENANT_NAME.auth0.com/",
+  AUTH0_AUDIENCE: "AUDIENCE (API IDENTIFIER)"
+};
